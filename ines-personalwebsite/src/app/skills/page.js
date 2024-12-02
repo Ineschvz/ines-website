b34f88e2afdc41b75e779e2cac9bb722285f0699
Ines Chavez <ineschvz21@gmail.com>
2024-12-02 15:37:11 -0500

    added tech images

--- ines-personalwebsite/src/app/skills/page.js
+++ ines-personalwebsite/src/app/skills/page.js
@@ -2,17 +2,19 @@
 
 import { DynaPuff } from "next/font/google";
 
+import Image from "next/image";
+
 
 const dynaPuff = DynaPuff({ subsets: ['latin'],  weight: ['400', '700']  });
 
 export default function Skills() {
   const languages = [
-    "TypeScript",
-    "JavaScript",
-    "HTML5",
-    "CSS",
-    "SQL",
-    "NoSQL (MongoDB)"
+    { name: "TypeScript", src: "/ts.png" },
+    { name: "JavaScript", src: "/js.png" },
+    { name: "HTML5", src: "/html5.png" },
+    { name: "CSS", src: "/css.png" },
+    { name: "SQL", src: "/sql.png" },
+    { name: "NoSQL (MongoDB)", src: "/mongodb.png" },
   ];
 
   const frameworksAndTechnologies = [
@@ -42,25 +44,24 @@ export default function Skills() {
       <div className="grid grid-cols-4 gap-6 items-center justify-center mt-12">
         <h1 className="text-2xl font-light text-rose-500 col-span-4">Languages</h1>
 
-        {languages.map((languages, index) => (
-          <div key={index} className="col-span-1 grid flex items-center">
-            <div className="grid"></div>
-            <div className="grid items-center justify-center bg-red-300 rounded p-4">
-              {languages}
+        {languages.map((language, index) => (
+          <div key={index} className="flex flex-row flex-wrap justify-center gap-6 my-5 w-full">
+            <div className="animate-bounce w-20 h-20 rounded-full p-4 flex items-center justify-center bg-red-300 hover:animate-spin transition duration-150 ease-in-out">
+              <Image src={language.src} alt={language.name} width={64} height={64} />
             </div>
           </div>
         ))}
       </div>
 
-      <div className="grid grid-cols-4 gap-6 items-center justify-center mt-12">
+      <div className="  grid grid-cols-4 gap-6 items-center justify-center mt-12">
         <h1 className="text-2xl font-light text-rose-500 col-span-4">Frameworks and Technologies</h1>
 
         {frameworksAndTechnologies.map((tech, index) => (
-          <div key={index} className="col-span-1">
-            <div className="p-6"></div>
-            <div className="flex items-center justify-center bg-red-300 rounded p-4">
+          <div key={index} className="flex flex-row flex-wrap justify-center gap-6 my-5 w-full">
+            <div className=" animate-bounce w-16 h-16 rounded-full p-4 flex items-center justify-center bg-red-300 hover:animate-spin transition duration-150 ease-in-out">
               {tech}
             </div>
+    
           </div>
         ))}
       </div>
