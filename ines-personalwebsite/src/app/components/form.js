c405cbdfc316a782007f8f63cd93d29ce36301a2
Ines Chavez <ineschvz21@gmail.com>
2025-05-07 18:09:07 -0400

    contact form is done and fully functioning

--- ines-personalwebsite/src/app/components/form.js
+++ ines-personalwebsite/src/app/components/form.js
@@ -10,6 +10,7 @@ export default function Form() {
 
     //creating the state variables fro the form 
 const [name, setName] = useState('');
+const [last, setLast] = useState('');
 const [email, setEmail] = useState(''); 
 const [message, setMessage] = useState('');
 
@@ -21,7 +22,7 @@ const handleSubmit = async (e) =>{
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ name, email, message }),
+        body: JSON.stringify({ name, last, email, message }),
     });
     if (res.ok) {
         alert('Email sent successfully');
@@ -35,36 +36,70 @@ const handleSubmit = async (e) =>{
 
 return (
 
-    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <div className='p-4 bg-gray-100 rounded-lg shadow-md flex flex-col gap-4'>
-            <h1 className="text-3xl font-bold text-center mb-4">
-                Contact Me 
+    <form onSubmit={handleSubmit} className="flex flex-col items-center gap-4 font-[ivyPresto]">
+        <div 
+            className='w-full flex justify-center' 
+            style={{ 
+                backgroundImage: "url('/InesTyping.jpg'), linear-gradient(to bottom, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.3))", 
+                backgroundBlendMode: 'overlay', 
+                backgroundSize: 'cover', 
+                backgroundPosition: 'center', 
+            }}
+        >
+        <div className='p-8 px-24 bg-orange-200/95 shadow-md flex flex-col gap-6 max-w-2xl w-full mx-auto min-h-screen items-center'>
+            <h1 className="font-medium text-4xl text-center mb-4 text-green-900">
+                Contact Me
             </h1>
-            <p className="text-center text-gray-600 mb-4">
+            <img src="/flower.png" alt="Flower" className="w-16 h-16 mx-auto" />
+            <p className="text-center text-green-900 mb-4 text-4xl font-bold">
                 If you have any questions or just want to say hi, feel free to reach out!
             </p>
 
-        <input 
-            type="text" 
-            placeholder="Name" 
-            value={name} 
-            onChange={(e) => setName(e.target.value)} 
-            className="border border-gray-300 rounded p-2"
-        />
-        <input 
-            type="email" 
-            placeholder="Email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-            className="border border-gray-300 rounded p-2"
-        />
-        <textarea 
-            placeholder="Message" 
-            value={message} 
-            onChange={(e) => setMessage(e.target.value)} 
-            className="border border-gray-300 rounded p-2"
-        ></textarea>
-        <button type="submit" className="bg-blue-500 text-white rounded p-2">Send</button>
+        <div className='flex flex-row gap-4 items-center justify-center w-full'>
+            <div className="w-full">
+                <h1 className="text-lg text-green-900">First Name</h1>
+                <input 
+                    type="text" 
+                    placeholder="First Name" 
+                    value={name} 
+                    onChange={(e) => setName(e.target.value)} 
+                    className="border border-gray-300 rounded p-2 w-full text-lg"
+                />
+            </div>
+            <div className="w-full">
+                <h1 className="text-lg text-green-900">Last Name</h1>
+                <input
+                    type="text" 
+                    placeholder="Last Name" 
+                    value={last} 
+                    onChange={(e) => setLast(e.target.value)} 
+                    className="border border-gray-300 rounded p-2 w-full text-lg"
+                />
+            </div>
+        </div>
+        <div className="w-full">
+            <h1 className="text-lg text-green-900">Email</h1>
+            <input 
+                type="email" 
+                placeholder="Email" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+                className="border border-gray-300 rounded p-2 w-full text-lg"
+            />
+        </div>
+        <div className="w-full">
+            <h1 className="text-lg text-green-900">Message</h1>
+            <textarea 
+                placeholder="Message" 
+                value={message} 
+                onChange={(e) => setMessage(e.target.value)} 
+                className="border border-gray-300 rounded p-2 w-full text-lg"
+            ></textarea>
+        </div>
+        <button type="submit" className="text-orange-600/90 rounded-full outline-white outline-double outline-4 p-3 px-10 text-xl">Send</button>
+        </div>
+
+        
         </div>
     </form>
 )
